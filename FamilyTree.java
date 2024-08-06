9a18527283b0fc65bc4ca1e7f8ddab4a0967067e
Darya Kauzova <amayasuzuki12@gmail.com>
2024-08-06 14:10:40 +0300

    First commit

---
+++ FamilyTree.java
@@ -0,0 +1,72 @@
+package ru.gb.family_tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FamilyTree {
+    private List<Human> familyTree = new ArrayList<Human>();
+
+
+
+    public void addHuman(Human human ){
+        familyTree.add(human);
+    }
+
+    public String getFamilyTree(){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Список членов семьи:\n");
+        for (Human human : familyTree){
+            stringBuilder.append(human);
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    public void removeHuman(Human human){
+        familyTree.remove(human);
+    }
+
+    // public String findChildren(FamilyTree tree){
+    //     Scanner scanner = new Scanner(System.in);
+    //     System.out.print("Enter a person: \n");
+    //     String person = scanner.nextLine();
+    //     scanner.close();
+    //     String child = "";
+    //     List<FamilyTree> childTree = new ArrayList<>();
+    //     for (Human human : familyTree) {
+    //         if (familyTree.contains(person)) {
+    //             child = human.getChildren();
+    //             for (Human human1 : familyTree) {
+    //                 if (familyTree.contains(child)) {
+    //                     childTree.addAll(human1);
+    //             }
+    //         }
+    //     }
+    // }
+
+    public String findChildren(FamilyTree tree) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a person: \n");
+        String person = scanner.nextLine();
+        scanner.close();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Human human : familyTree) {
+            if (human.getName().equals(person)) {
+                List<String> children = new ArrayList<>();
+                if (!children.isEmpty()) {
+                    stringBuilder.append("Children of ").append(person).append(":\n");
+                    for (String child : children) {
+                        stringBuilder.append(child).append("\n");
+                    }
+                } else {
+                    stringBuilder.append(person).append(" has no children.\n");
+                }
+                break;
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+
+}
